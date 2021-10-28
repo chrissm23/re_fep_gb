75c7dc5cc5d24714a4813e44755cfc7c00bb3d9e
chrissm23 <ccsm983@gmail.com>
2021-10-28 17:52:23 +0200

    added line to copy minimization restart file

--- setup/tmpls/minimize_tmpl/minimize.sh
+++ setup/tmpls/minimize_tmpl/minimize.sh
@@ -13,4 +13,6 @@ mpirun -n 6 $pmemd -i $fe_dir/minimization.in -p $fe_dir/topology.parm7 \
 	-O -o $fe_dir/minimization.out -inf $fe_dir/min.info \
 	-r $fe_dir/minimization.rst7
 
+cp $fe_dir/minimization.rst7 ./FE/equilibration/%wt_or_mt%/minimization.rst7
+
 echo "Minimization finished."
