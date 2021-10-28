0048d0bece23eecae3bf5a3314652f91ce389e3b
chrissm23 <ccsm983@gmail.com>
2021-10-28 16:04:13 +0200

    reduced number of cores used to minimize

--- setup/tmpls/minimize_tmpl/minimize.sh
+++ setup/tmpls/minimize_tmpl/minimize.sh
@@ -8,7 +8,7 @@ fe_dir=./FE/minimization/%wt_or_mt%
 
 echo "Minimizing..."
 
-mpirun -n 8 $pmemd -i $fe_dir/minimization.in -p $fe_dir/topology.parm7 \
+mpirun -n 6 $pmemd -i $fe_dir/minimization.in -p $fe_dir/topology.parm7 \
 	-c $fe_dir/coordinates.rst7 -ref $fe_dir/coordinates.rst7 \
 	-O -o $fe_dir/minimization.out -inf $fe_dir/min.info \
 	-r $fe_dir/minimization.rst7
